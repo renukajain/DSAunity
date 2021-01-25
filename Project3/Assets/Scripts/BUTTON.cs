68064969704a68907edec1ba54526792938d3bca
Javier Salmeron Rodriguez <javiesal>
2021-01-26 00:37:05 +0100

    inventario abre y cierra. Falta perfilar inventario + tienda y decorar. (Implementacion android pendiente)

---
+++ Project3/Assets/Scripts/BUTTON.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BUTTON : MonoBehaviour
+{
+    int counter = 0;
+    public GameObject Panel;
+    // Start is called before the first frame update
+    public void TriggerInventory()
+    {
+       counter++;
+       if (counter%2==1)
+            Panel.gameObject.SetActive(true);
+       else
+            Panel.gameObject.SetActive(false);
+
+        
+    }
+}
